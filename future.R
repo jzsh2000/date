a4a15fa64567326b58e0fb5c10dd8e55e558a0a4
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2018-01-11 13:42:11 +0800

    Check input value

--- future.R
+++ future.R
@@ -1,14 +1,27 @@
 dates = commandArgs(TRUE)
 
+error <- FALSE
 if (length(dates) == 0) {
     glue::glue('Rscript future.R <date_base> <dates>')
 } else if (length(dates) == 1) {
-    if (file.exists('BASE')) {
-        date_base = readLines('BASE')
-        glue::glue("{lubridate::ymd(date_base) + as.integer(dates[1])}")
+    if (file.exists('BASE') && stringr::str_detect(dates[1], '^-?[1-9][0-9]*$')) {
+        date_base = lubridate::ymd(readLines('BASE'))
+        date_offset = as.integer(dates[1])
     } else {
-        glue::glue('Rscript future.R <date_base> <dates>')
+        error = TRUE
     }
 } else {
-    glue::glue("{lubridate::ymd(dates[1]) + as.integer(dates[2])}")
+    if (stringr::str_detect(dates[2], '^-?[1-9][0-9]*$')) {
+        date_base = lubridate::ymd(dates[1])
+        date_offset = as.integer(dates[2])
+    } else {
+        error = TRUE
+    }
+}
+
+if (!error) {
+    glue::glue("{lubridate::ymd(date_base) + as.integer(date_offset)}")
+} else {
+    glue::glue('Rscript future.R <date_base> <date_offset>
+               e.g. Rscript future.R 1990-01-01 10000')
 }
